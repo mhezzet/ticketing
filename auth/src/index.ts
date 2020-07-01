7d351c147074bf584d231f226c59fe163daac9f1
ezzat <ezzat@garment.io>
2020-07-01 20:20:34 +0200

    startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,7 +8,7 @@ if (!process.env.JWT_KEY) {
 if (!process.env.MONGO_URI) {
   throw new Error('MONGO_URI must be defined')
 }
-
+console.log('💨starting up...')
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
